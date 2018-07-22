b8278537df7b32f6242601b499856316511aad44
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 07:11:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,23 +29,23 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	# sets the 1st key of the :rent_in_dollars hash to a symbol, :one_piece_owned, whose va
-	# lue is the integer 25 (FAILED - 4)
-	#       sets the 2nd key of the :rent_in_dollars hash to a symbol, :two_pieces_owned, whose v
-	# alue is the integer 50 (FAILED - 5)
-	#       sets the 3rd key of the :rent_in_dollars hash to a symbol, :three_pieces_owned, whose
-	#  value is the integer 100 (FAILED - 6)
-	#       sets the 4th key of the :rent_in_dollars hash to a symbol, :four_pieces_owned, whose
-	# value is the integer 200 (FAILED - 7)
-	#       sets the 1st key of the :names hash to a symbol, :reading_railroad, whose value is an
-	#  empty hash (FAILED - 8)
-	#       sets the 2nd key of the :names hash to a symbol, :pennsylvania_railroad, whose value
-	# is an empty hash (FAILED - 9)
-	#       sets the 3rd key of the :names hash to a symbol, :b_and_o_railroad, whose value is an
-	#  empty hash (FAILED - 10)
-	#       sets the 4th key of the :names hash to a symbol, :shortline, whose value is an empty
-	# hash (FAILED - 11)
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:names => {
+				:reading_railroad => {},
+				:pennsylvania_railroad => {},
+				:b_and_o_railroad => {},
+				:shortline => {},
+			},
+			:rent_in_dollars => {
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200,
+			},
+		},
+	}
 end
 
 def monopoly_with_fourth_tier
