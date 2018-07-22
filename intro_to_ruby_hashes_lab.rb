a6ba0223c5379d0b98033ebf6696748cc4e48129
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-22 07:14:03 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -74,13 +74,4 @@ def monopoly_with_fourth_tier
 			},
 		},
 	}
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-# 	sets the 1st key of the :reading_railroad hash to a string, 'mortgage_value', whose v
-# alue is '$100' (FAILED - 12)
-# 		sets the 1st key of the :pennsylvania_railroad hash to a string, 'mortgage_value', wh
-# ose value is '$200' (FAILED - 13)
-# 		sets the 1st key of the :b_and_o_railroad hash to a string, 'mortgage_value', whose v
-# alue is '$400' (FAILED - 14)
-# 		sets the 1st key of the :shortline hash to a string, `mortgage_value`, whose value is
-# '$800' (FAILED - 15)
 end
